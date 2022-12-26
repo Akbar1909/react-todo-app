f6cf4c504483494df3c9597fee9644a95c83c4be
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2022-12-26 10:31:08 +0500

    again check github actions

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 export default function HomePage() {
   return <div>
   <h1>Hello World</h1>
-  <p>Github actions are created</p>
+  <p>Github actions are created 123</p>
   <Link to="about">About Us</Link>
 </div>
 }
