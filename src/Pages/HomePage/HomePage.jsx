64c277636d7dede30588e55bf8cb7998a543fc96
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2022-12-26 10:23:45 +0500

    check github actions

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 export default function HomePage() {
   return <div>
   <h1>Hello World</h1>
+  <p>Github actions are created</p>
   <Link to="about">About Us</Link>
 </div>
 }
