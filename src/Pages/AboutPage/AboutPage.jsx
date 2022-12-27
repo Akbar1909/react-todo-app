56a99f0fa4d9a9e8e77e8f9fff31a50d717d3dce
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2022-12-27 18:16:04 +0500

    check

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function AboutPage() {
   return (
-    <div>Let's change about page test 123</div>
+    <div>Let's change about page test 123 567</div>
   )
 }
