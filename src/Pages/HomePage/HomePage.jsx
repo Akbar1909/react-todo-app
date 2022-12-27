665fb5058c43d19fe1668fb8be7201d0e314c479
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2022-12-27 09:28:20 +0500

    add 123 to home page

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,8 +3,9 @@ import {Link} from 'react-router-dom';
 
 export default function HomePage() {
   return <div>
-  <h1>Hello World</h1>
+  <h1>Hello World 123</h1>
   <input type="text" />
+  
   <p>Github actions are created 123</p>
   <Link to="about">About Us</Link>
 </div>
