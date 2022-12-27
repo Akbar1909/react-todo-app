e46304beb68ee1390628400093386e7fb6b05418
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2022-12-27 08:23:16 +0500

    add input to home page

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 export default function HomePage() {
   return <div>
   <h1>Hello World</h1>
+  <input type="text" />
   <p>Github actions are created 123</p>
   <Link to="about">About Us</Link>
 </div>
